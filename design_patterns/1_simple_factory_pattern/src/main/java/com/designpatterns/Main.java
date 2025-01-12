3b8cf8bc5f61c5e3c056def7ac974a313cf5b750
dineshkumar51 <ncpdd51@gmail.com>
2025-01-12 20:02:48 +0530

    renamed modules

--- design_patterns/1_simple_factory_pattern/src/main/java/com/designpatterns/Main.java
+++ design_patterns/1_simple_factory_pattern/src/main/java/com/designpatterns/Main.java
@@ -2,7 +2,12 @@ package com.designpatterns;
 
 import com.designpatterns.simplefactory.AnimalFactory;
 import com.designpatterns.simplefactory.Animals.Animal;
-
+/*
+It creates an object without exposing the instantiation logic to the client.
+The factory pattern provides a centralized point of control for object creation, which makes maintenance and testing easier.
+The factory pattern promotes code reusability by defining common creation logic in superclass methods
+The factory pattern makes it easy to add new product types without modifying existing client code.
+ */
 public class Main {
 
     public static void main(String[] args) {
--- design_patterns/2_factory_method_pattern/src/main/java/com/designpatterns/Main.java
+++ design_patterns/2_factory_method_pattern/src/main/java/com/designpatterns/Main.java
@@ -3,7 +3,10 @@ package com.designpatterns;
 import com.designpatterns.factorymethod.factory.AnimalFactory;
 import com.designpatterns.factorymethod.factory.DogFactory;
 import com.designpatterns.factorymethod.factory.TigerFactory;
-
+/*
+It defines an interface for creating an object, but lets subclasses decide which class to instantiate.
+The Factory Method pattern lets a class defer instantiation to subclasses.
+ */
 public class Main {
 
     public static void main(String[] args) {
